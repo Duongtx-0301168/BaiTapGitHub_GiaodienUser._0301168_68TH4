6c8568edc7ee052d98e671b3399bbfc7a49749ba
duongtx_0301168 <xuanduongtrinh1007@gmail.com>
2026-01-29 18:24:10 +0700

    Thêm User và Viewmodelbase

---
+++ User/User.cs
@@ -0,0 +1,17 @@
+namespace GiaodienUser.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public User()
+        {
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
